d90471f6d2575c0143fac32a5d9c00dcc689de4f
MoutoJohn <177659938+MoutoJohn@users.noreply.github.com>
2026-09-07 00:04:33 +0300

    add add_task function

--- src/task_manager.py
+++ src/task_manager.py
@@ -3,5 +3,9 @@ from task import Task
 class TaskManager:
     def __init__(self):
         self._tasks = {}
+    
+    def add_task(self, title):
+        task = Task(title)        
+        self._tasks[task.task_id] = task 
+        return task
         
-
