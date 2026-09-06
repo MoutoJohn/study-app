9f1a86dd7f019a37cb57abb399bf21fadbcecd8d
MoutoJohn <177659938+MoutoJohn@users.noreply.github.com>
2026-09-07 00:39:04 +0300

    add toggle_task plus comments on return values

--- src/task_manager.py
+++ src/task_manager.py
@@ -9,11 +9,17 @@ class TaskManager:
         self._tasks[task.task_id] = task 
         return task
         
-    def remove_task(self, task_id):
-        return self._tasks.pop(task_id)
+    def remove_task(self, task_id):         #returns an error if fail
+        return self._tasks.pop(task_id)     
         
-    def get_task(self, task_id):
-        return self._tasks.get(task_id)
+    def get_task(self, task_id):            #returns None if fail
+        return self._tasks.get(task_id)     
     
     def get_all_tasks(self):
         return list(self._tasks.values())
+    
+    def toggle_task(self, task_id):         #returns None if fail
+        task = self.get_task(self, task_id)
+        if task:
+            task.toggle()
+        return task
